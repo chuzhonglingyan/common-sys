5a6f45fa9ebb3af4b611babb655d1b55193afb8e
yuntian <944610721@qq.com>
2020-02-03 21:45:37 +0800

    创建web通用应用

---
+++ sys-web/src/main/java/com/yuntian/sys/SysApplication.java
@@ -0,0 +1,20 @@
+package com.yuntian.sys;
+
+import com.alibaba.nacos.spring.context.annotation.config.NacosPropertySource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Administrator
+ */
+@NacosPropertySource(dataId = "redis", groupId= "sys-web",  autoRefreshed = true)
+@NacosPropertySource(dataId = "data-source", groupId= "sys-web",  autoRefreshed = true)
+@SpringBootApplication
+public class SysApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SysApplication.class, args);
+    }
+
+}
